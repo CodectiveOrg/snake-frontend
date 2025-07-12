bbe8e66aa0a09166821ea7e91c46586b129f1af4
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-12 16:33:05 +0330

    [ADD] collision detection

--- src/Entities/game-master.ts
+++ src/Entities/game-master.ts
@@ -2,12 +2,16 @@ import { Snake } from "./snake.ts";
 import { Food } from "./food.ts";
 import { Canvas } from "../components/canvas.ts";
 import { Controller } from "./controller.ts";
+import { DOM } from "../utils/dom.utils.ts";
 
 export class GameMaster {
   public controller: Controller;
   public canvas: Canvas;
   public food: Food;
   public snake: Snake;
+
+  private score: number = 0;
+  private isGameOver: boolean = false;
 
   public constructor() {
     this.controller = new Controller();
@@ -23,7 +27,11 @@ export class GameMaster {
   private initGameLoop(): void {
     let lastTime = Date.now();
 
-    const update = (): void => {
+    const frameCallback = (): void => {
+      if (this.isGameOver) {
+        return;
+      }
+
       const now = Date.now();
 
       const deltaTime = now - lastTime;
@@ -31,25 +39,38 @@ export class GameMaster {
 
       const distance = deltaTime * Snake.SPEED;
 
-      const result = this.draw(distance);
-      if (!result) {
-        return;
-      }
+      this.update(distance);
 
-      requestAnimationFrame(update);
+      requestAnimationFrame(frameCallback);
     };
 
-    requestAnimationFrame(update);
+    requestAnimationFrame(frameCallback);
   }
 
-  private draw(distance: number): boolean {
+  private update(distance: number): void {
+    this.snake.move(distance);
+    this.checkForGameOver();
+    this.checkForFood();
+    this.draw();
+  }
+
+  private checkForGameOver(): void {
+    this.isGameOver =
+      this.snake.doesCollideWithWall() || this.snake.doesCollideWithItself();
+  }
+
+  private checkForFood(): void {
+    if (this.snake.doesCollideWithFood(this.food.coords)) {
+      this.score++;
+      DOM.score.textContent = this.score.toString();
+
+      this.food.generateFood();
+    }
+  }
+
+  private draw(): void {
     this.canvas.clear();
-
     this.canvas.drawFood(this.food.coords);
-
-    const result = this.snake.move(distance);
     this.canvas.drawSnake();
-
-    return result;
   }
 }
--- src/Entities/snake.ts
+++ src/Entities/snake.ts
@@ -1,15 +1,27 @@
 import type { Point } from "../types/point.ts";
 import { Controller } from "./controller.ts";
+import { Canvas } from "../components/canvas.ts";
 
 export class Snake {
   public static readonly SPEED = 16 / 1000;
 
   public body: Point[] = [
     { x: 0, y: 0 },
-    { x: 4, y: 0 },
+    { x: 10, y: 0 },
   ];
 
   public constructor(private controller: Controller) {}
+
+  public get head(): Point {
+    return this.body.at(-1)!;
+  }
+
+  public get intHead(): Point {
+    return {
+      x: Math.floor(this.head.x),
+      y: Math.floor(this.head.y),
+    };
+  }
 
   private moveBody(index: number, direction: string, distance: number): void {
     const { x, y } = this.body[index];
@@ -61,22 +73,24 @@ export class Snake {
   }
 
   private moveToTurningPoint(distance: number): number {
-    const { x, y } = this.body.at(-1)!;
-
     let remainingDistanceToWholeNumber = 0;
 
     switch (this.controller.direction) {
       case "KeyS":
-        remainingDistanceToWholeNumber = Math.floor(y + distance) - y;
+        remainingDistanceToWholeNumber =
+          Math.floor(this.head.y + distance) - this.head.y;
         break;
       case "KeyW":
-        remainingDistanceToWholeNumber = y - Math.ceil(y - distance);
+        remainingDistanceToWholeNumber =
+          this.head.y - Math.ceil(this.head.y - distance);
         break;
       case "KeyD":
-        remainingDistanceToWholeNumber = Math.floor(x + distance) - x;
+        remainingDistanceToWholeNumber =
+          Math.floor(this.head.x + distance) - this.head.x;
         break;
       default:
-        remainingDistanceToWholeNumber = x - Math.ceil(x - distance);
+        remainingDistanceToWholeNumber =
+          this.head.x - Math.ceil(this.head.x - distance);
     }
 
     if (remainingDistanceToWholeNumber <= 0) {
@@ -91,94 +105,75 @@ export class Snake {
 
     this.recur(remainingDistanceToWholeNumber);
 
-    this.body.push({ ...this.body.at(-1)! });
+    this.body.push({ ...this.head });
 
     this.controller.consume();
 
     return remainingDistanceToWholeNumber;
   }
 
-  public move(distance: number): boolean {
+  public move(distance: number): void {
     if (this.controller.requestedDirection) {
-      const consumedDistance = this.moveToTurningPoint(distance);
-      distance -= consumedDistance;
+      const travelledDistance = this.moveToTurningPoint(distance);
+      distance -= travelledDistance;
     }
 
     this.moveBody(this.body.length - 1, this.controller.direction, distance);
     this.recur(distance);
-
-    // if (shouldSnakeStayAtSameSize) {
-    //   this.updateSnake();
-    // }
-    //
-    // const newCell = this.newHead();
-    //
-    // if (this.didSnakeEatItself(newCell) || this.didSnakeHitWall(newCell)) {
-    //   return false;
-    // } else {
-    //   this.goAhead(newCell);
-    // }
-    //
-    // if (this.isFoodEaten(snake.at(-1))) {
-    //   scoreCounter++;
-    //   score.textContent = scoreCounter.toString();
-    //
-    //   this.food.generateFood();
-    //
-    //   shouldSnakeStayAtSameSize = false;
-    // } else {
-    //   shouldSnakeStayAtSameSize = true;
-    // }
-
-    return true;
   }
 
-  // private isFoodEaten({ x, y }: Point): boolean {
-  //   return x === food.x && y === food.y;
-  // }
-  //
-  // private goAhead(newCell) {
-  //   if (isThereNewDirection) {
-  //     snakeNodes.push(newCell);
-  //   } else {
-  //     snakeNodes[snakeNodes.length - 1] = newCell;
-  //   }
-  // }
-  //
-  // private updateSnake(): void {
-  //   const tail = this.body[0];
-  //   const yestertail = this.body[0];
-  //
-  //   if (tail.x === yestertail.x) {
-  //     if (tail.y > yestertail.y) {
-  //       tail.y--;
-  //     } else {
-  //       tail.y++;
-  //     }
-  //   } else {
-  //     if (tail.x > yestertail.x) {
-  //       tail.x--;
-  //     } else {
-  //       tail.x++;
-  //     }
-  //   }
-  //
-  //   if (JSON.stringify(tail) === JSON.stringify(yestertail)) {
-  //     this.body.shift();
-  //   }
-  // }
-  //
-  // private didSnakeEatItself(head: Point): boolean {
-  //   for (let i = 0; i < this.body.length - 1; i++) {
-  //     if (this.body[i].x === head.x && this.body[i].y === head.y) {
-  //       return true;
-  //     }
-  //   }
-  //
-  //   return false;
-  // }
-  //
-  // private didSnakeHitWall(head: Point): boolean {
-  //   return head.x < 0 || head.y < 0 || head.x > 29 || head.y > 19;
-  // }
+  public doesCollideWithFood(food: Point): boolean {
+    return this.doesTwoPointsCollide(this.intHead, food);
+  }
+
+  public doesCollideWithItself(): boolean {
+    for (let i = 1; i <= this.body.length - 3; i++) {
+      if (
+        this.doesPointCollideWithLine(
+          this.intHead,
+          this.body[i],
+          this.body[i - 1],
+        )
+      ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public doesCollideWithWall(): boolean {
+    return (
+      this.head.x < 0 ||
+      this.head.y < 0 ||
+      this.head.x >= Canvas.BOARD_WIDTH ||
+      this.head.y >= Canvas.BOARD_HEIGHT
+    );
+  }
+
+  private doesTwoPointsCollide(point1: Point, point2: Point): boolean {
+    return Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y) < 1;
+  }
+
+  private doesPointCollideWithLine(
+    point1: Point,
+    point2: Point,
+    point3: Point,
+  ): boolean {
+    if (point2.x === point3.x) {
+      return (
+        point1.x === point2.x &&
+        Math.min(point2.y, point3.y) <= point1.y &&
+        point1.y <= Math.max(point2.y, point3.y)
+      );
+    } else if (point2.y === point3.y) {
+      return (
+        point1.y === point2.y &&
+        Math.min(point2.x, point3.x) <= point1.x &&
+        point1.x <= Math.max(point2.x, point3.x)
+      );
+    }
+
+    return false;
+  }
 }
