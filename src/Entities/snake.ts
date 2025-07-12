0a9fb1b3d0cab06829de483ee9bbdf101e5e3417
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-12 18:19:44 +0330

    [REFACTOR] snake

--- src/Entities/snake.ts
+++ src/Entities/snake.ts
@@ -2,8 +2,12 @@ import type { Point } from "../types/point.ts";
 import { Controller } from "./controller.ts";
 import { Canvas } from "../components/canvas.ts";
 import {
+  calculateDirection,
+  calculateDistance,
   doesPointCollideWithLine,
   doesPointCollideWithPoint,
+  findDistanceToTurn,
+  movePoint,
 } from "../utils/geometry.utils.ts";
 
 export class Snake {
@@ -27,103 +31,14 @@ export class Snake {
     };
   }
 
-  private moveBody(index: number, direction: string, distance: number): void {
-    const { x, y } = this.body[index];
-
-    switch (direction) {
-      case "KeyS":
-        this.body[index] = { x, y: y + distance };
-        break;
-      case "KeyW":
-        this.body[index] = { x, y: y - distance };
-        break;
-      case "KeyD":
-        this.body[index] = { x: x + distance, y };
-        break;
-      default:
-        this.body[index] = { x: x - distance, y };
-    }
-  }
-
-  private recur(totalDistance: number): void {
-    const currentDistance = Math.abs(
-      this.body[0].x === this.body[1].x
-        ? this.body[1].y - this.body[0].y
-        : this.body[1].x - this.body[0].x,
-    );
-
-    if (currentDistance > totalDistance) {
-      this.moveBody(0, this.dir(0, 1), totalDistance);
-    } else {
-      this.body.shift();
-      this.recur(totalDistance - currentDistance);
-    }
-  }
-
-  private dir(firstIndex: number, secondIndex: number): string {
-    if (this.body[secondIndex].y - this.body[firstIndex].y > 0) {
-      return "KeyS";
-    }
-
-    if (this.body[secondIndex].y - this.body[firstIndex].y < 0) {
-      return "KeyW";
-    }
-
-    if (this.body[secondIndex].x - this.body[firstIndex].x > 0) {
-      return "KeyD";
-    }
-
-    return "KeyA";
-  }
-
-  private moveToTurningPoint(distance: number): number {
-    let remainingDistanceToWholeNumber: number;
-
-    switch (this.controller.direction) {
-      case "KeyS":
-        remainingDistanceToWholeNumber =
-          Math.floor(this.head.y + distance) - this.head.y;
-        break;
-      case "KeyW":
-        remainingDistanceToWholeNumber =
-          this.head.y - Math.ceil(this.head.y - distance);
-        break;
-      case "KeyD":
-        remainingDistanceToWholeNumber =
-          Math.floor(this.head.x + distance) - this.head.x;
-        break;
-      default:
-        remainingDistanceToWholeNumber =
-          this.head.x - Math.ceil(this.head.x - distance);
-    }
-
-    if (remainingDistanceToWholeNumber <= 0) {
-      return 0;
-    }
-
-    this.moveBody(
-      this.body.length - 1,
-      this.controller.direction,
-      remainingDistanceToWholeNumber,
-    );
-
-    this.recur(remainingDistanceToWholeNumber);
-
-    this.body.push({ ...this.head });
-
-    this.controller.consume();
-
-    return remainingDistanceToWholeNumber;
-  }
-
   public move(distance: number): void {
     if (this.controller.requestedDirection) {
       const travelledDistance = this.moveToTurningPoint(distance);
       distance -= travelledDistance;
     }
 
-    this.moveBody(this.body.length - 1, this.controller.direction, distance);
-    this.recur(distance);
+    this.moveHead(distance);
+    this.moveTail(distance);
   }
 
   public doesCollideWithPoint(point: Point, offset: number = 1): boolean {
@@ -151,5 +66,45 @@ export class Snake {
       this.head.x >= Canvas.BOARD_WIDTH ||
       this.head.y >= Canvas.BOARD_HEIGHT
     );
+  }
+
+  private moveToTurningPoint(distance: number): number {
+    const distanceToTurn = findDistanceToTurn(
+      this.head,
+      this.controller.direction,
+    );
+
+    if (distanceToTurn > distance) {
+      return 0;
+    }
+
+    this.moveHead(distanceToTurn);
+    this.moveTail(distanceToTurn);
+
+    this.body.push({ ...this.head });
+
+    this.controller.consume();
+
+    return distanceToTurn;
+  }
+
+  private moveHead(distance: number): void {
+    this.body[this.body.length - 1] = movePoint(
+      this.body[this.body.length - 1],
+      this.controller.direction,
+      distance,
+    );
+  }
+
+  private moveTail(totalDistance: number): void {
+    const distanceToNextPoint = calculateDistance(this.body[0], this.body[1]);
+
+    if (totalDistance < distanceToNextPoint) {
+      const direction = calculateDirection(this.body[0], this.body[1]);
+      this.body[0] = movePoint(this.body[0], direction, totalDistance);
+    } else {
+      this.body.shift();
+      this.moveTail(totalDistance - distanceToNextPoint);
+    }
   }
 }
--- src/utils/geometry.utils.ts
+++ src/utils/geometry.utils.ts
@@ -1,4 +1,55 @@
 import type { Point } from "../types/point.ts";
+import type { Direction } from "../types/direction.ts";
+
+export function movePoint(
+  point: Point,
+  direction: Direction,
+  distance: number,
+): Point {
+  switch (direction) {
+    case "KeyW":
+      return { ...point, y: point.y - distance };
+    case "KeyA":
+      return { ...point, x: point.x - distance };
+    case "KeyS":
+      return { ...point, y: point.y + distance };
+    case "KeyD":
+      return { ...point, x: point.x + distance };
+  }
+}
+
+export function findDistanceToTurn(point: Point, direction: Direction): number {
+  switch (direction) {
+    case "KeyW":
+      return point.y - Math.floor(point.y);
+    case "KeyA":
+      return point.x - Math.floor(point.x);
+    case "KeyS":
+      return Math.ceil(point.y) - point.y;
+    case "KeyD":
+      return Math.ceil(point.x) - point.x;
+  }
+}
+
+export function calculateDistance(point1: Point, point2: Point): number {
+  return Math.abs(point2.x - point1.x) + Math.abs(point2.y - point1.y);
+}
+
+export function calculateDirection(point1: Point, point2: Point): Direction {
+  if (point2.y < point1.y) {
+    return "KeyW";
+  }
+
+  if (point2.x < point1.x) {
+    return "KeyA";
+  }
+
+  if (point1.y < point2.y) {
+    return "KeyS";
+  }
+
+  return "KeyD";
+}
 
 export function doesPointCollideWithPoint(
   point1: Point,
