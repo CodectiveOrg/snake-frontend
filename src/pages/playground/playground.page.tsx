1934793b09d6f4ed23edfddd294bb1bba021fe77
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-18 11:13:27 +0330

    [ADD] playground page

---
+++ src/pages/playground/playground.page.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import styles from "./playground.module.css";
+
+export default function PlaygroundPage(): ReactNode {
+  return <div className={styles.playground}></div>;
+}
