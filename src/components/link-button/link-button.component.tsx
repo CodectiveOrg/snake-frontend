90997e47d6f883a0f4394d79f1477f431bbefd7b
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-19 19:18:27 +0330

    [FIX] parent's classname overrides component's classname

--- src/components/link-button/link-button.component.tsx
+++ src/components/link-button/link-button.component.tsx
@@ -2,10 +2,20 @@ import type { ReactNode } from "react";
 
 import { NavLink, type NavLinkProps } from "react-router";
 
+import clsx from "clsx";
+
 import styles from "./link-button.module.css";
 
 type Props = NavLinkProps;
 
-export default function LinkButtonComponent(props: Props): ReactNode {
-  return <NavLink className={styles["link-button"]} {...props}></NavLink>;
+export default function LinkButtonComponent({
+  className,
+  ...otherProps
+}: Props): ReactNode {
+  return (
+    <NavLink
+      className={clsx(styles["link-button"], className)}
+      {...otherProps}
+    />
+  );
 }
