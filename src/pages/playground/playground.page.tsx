e97e1ff3a15363f405ec1119460eb5ebec6aac1a
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-19 23:32:37 +0330

    [ADD] table

--- src/pages/playground/playground.page.tsx
+++ src/pages/playground/playground.page.tsx
@@ -1,17 +1,78 @@
-import { type ReactNode, useRef } from "react";
+import { type ReactNode } from "react";
 
-import CanvasComponent from "@/components/canvas/canvas.component.tsx";
+import TableComponent from "@/components/table/table.component.tsx";
 
 import styles from "./playground.module.css";
 
 export default function PlaygroundPage(): ReactNode {
-  const ref = useRef<HTMLCanvasElement>(null);
-
   return (
     <div className={styles.playground}>
       <h1>Playground</h1>
       <main style={{ display: "grid", gap: "1rem", width: "75rem" }}>
-        <CanvasComponent ref={ref} />
+        <TableComponent
+          items={[
+            {
+              rank: 1,
+              username: "BijanProgrammer",
+              todayHighScore: 23,
+              totalHighScore: 815,
+            },
+            {
+              rank: 2,
+              username: "AK47",
+              todayHighScore: 0,
+              totalHighScore: 316,
+            },
+            {
+              rank: 3,
+              username: "SMAmini",
+              todayHighScore: 0,
+              totalHighScore: 108,
+            },
+            {
+              rank: 4,
+              username: "ZZare",
+              todayHighScore: 8,
+              totalHighScore: 42,
+            },
+            {
+              rank: 5,
+              username: "MAli",
+              todayHighScore: 0,
+              totalHighScore: 23,
+            },
+            {
+              rank: 6,
+              username: "MAliKH",
+              todayHighScore: 15,
+              totalHighScore: 16,
+            },
+            {
+              rank: 7,
+              username: "Mhmmdi",
+              todayHighScore: 4,
+              totalHighScore: 15,
+            },
+            {
+              rank: 8,
+              username: "Mazaheri",
+              todayHighScore: 0,
+              totalHighScore: 8,
+            },
+            {
+              rank: 9,
+              username: "Vishk",
+              todayHighScore: 0,
+              totalHighScore: 4,
+            },
+            {
+              rank: 10,
+              username: "AEghtedar",
+              todayHighScore: 0,
+              totalHighScore: 0,
+            },
+          ]}
+        />
       </main>
     </div>
   );
