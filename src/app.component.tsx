15a2f20ce3fd2b83a14ea8faa7229003e0e55123
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-20 10:13:07 +0330

    [ADD] react query

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -4,14 +4,16 @@ import { Toaster } from "sonner";
 
 import RouterComponent from "@/components/router/router.component.tsx";
 
+import QueryProvider from "@/providers/query.provider.tsx";
+
 import "./app.module.css";
 
 function AppComponent(): ReactNode {
   return (
-    <>
+    <QueryProvider>
       <RouterComponent />
       <Toaster />
-    </>
+    </QueryProvider>
   );
 }
 
