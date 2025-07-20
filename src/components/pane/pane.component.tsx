d1973877a12bb73966d908fbfa93f4bc6c1e8d93
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-20 18:46:06 +0330

    [ADD] shade to pane

--- src/components/pane/pane.component.tsx
+++ src/components/pane/pane.component.tsx
@@ -3,25 +3,28 @@ import type { CSSProperties, PropsWithChildren, ReactNode } from "react";
 import clsx from "clsx";
 
 import PaneFrameComponent from "@/components/pane/components/pane-frame/pane-frame.component.tsx";
+import PaneShadeComponent from "@/components/pane/components/pane-shade/pane-shade.component.tsx";
 
 import { Size } from "@/configs/size.config.ts";
 
-import { useResizeObserver } from "@/hooks/use-resize-observer.ts";
+import { useResizeObserverHook } from "@/hooks/use-resize-observer.hook.ts";
 
 import styles from "./pane.module.css";
 
 type Props = PropsWithChildren<{
   title: string;
+  shade?: boolean;
   className?: string;
 }>;
 
 export default function PaneComponent({
   title,
+  shade = false,
   className,
   children,
 }: Props): ReactNode {
   const { ref: titleRef, width: titleWidth } =
-    useResizeObserver<HTMLDivElement>();
+    useResizeObserverHook<HTMLDivElement>();
 
   const minBleed =
     Size.PANE_MIN_BLEED +
@@ -42,6 +45,9 @@ export default function PaneComponent({
       }
     >
       <PaneFrameComponent className={styles.frame} titleWidth={titleWidth} />
+      {shade && (
+        <PaneShadeComponent className={styles.shade} titleWidth={titleWidth} />
+      )}
       <div ref={titleRef} className={styles.title}>
         {title}
       </div>
