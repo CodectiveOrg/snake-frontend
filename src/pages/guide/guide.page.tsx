563e14019cc6023ffe5424d47d8f055831ea127c
SinaMAmini <smobasheramini@gmail.com>
2025-07-21 14:09:41 +0330

    [REWRITE] explaining

--- src/pages/guide/guide.page.tsx
+++ src/pages/guide/guide.page.tsx
@@ -9,28 +9,28 @@ export default function GuidePage() {
         <div className={style.guide}>
           <div className={style["navigation-keys"]}>
             <img src="./../../../public/images/guide/wads.svg" alt="wasd" />
-            <span className="explain">Navigation keys</span>
+            <span className="explain">NAVIGATION KEYS</span>
           </div>
           <div className={style["esc-key"]}>
             <img
               src="./../../../public/images/guide/esc%20button.svg"
               alt="button"
             />
-            <span className="explain">Access Game menu</span>
+            <span className="explain">ACCESS GAME MENU</span>
           </div>
           <div className={style["move-with-touch"]}>
             <img
               src="./../../../public/images/guide/TOUCH%20SCREEN%20CONTROLL.svg"
               alt="touch"
             />
-            <span className="explain">Move with touch</span>
+            <span className="explain">MOVE WITH TOUCH</span>
           </div>
           <div className={style["pause-game"]}>
             <img
               src="./../../../public/images/guide/pause%20btn.svg"
               alt="pause"
             />
-            <span className="explain">Pause the Game</span>
+            <span className="explain">PAUSE THE GAME</span>
           </div>
         </div>
       </PaneComponent>
