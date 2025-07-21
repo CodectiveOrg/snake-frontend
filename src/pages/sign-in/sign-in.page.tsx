118cd794009da2019558bf4c29f5a1b83677ddcc
ZahraZare <zahrazare3099@gmail.com>
2025-07-21 20:15:55 +0330

    Refactor design and add logic

--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -1,4 +1,10 @@
-import { type ReactNode, useState } from "react";
+import { type FormEvent, type ReactNode, useState } from "react";
+
+import { useNavigate } from "react-router";
+
+import { useMutation } from "@tanstack/react-query";
+
+import { toast } from "sonner";
 
 import ButtonComponent from "@/components/button/button.component";
 import CheckboxComponent from "@/components/checkbox/checkbox.component.tsx";
@@ -6,12 +12,52 @@ import LinkButtonComponent from "@/components/link-button/link-button.component"
 import PaneComponent from "@/components/pane/pane.component";
 import TextInputComponent from "@/components/text-input/text-input.component";
 
+import type { SigninDto } from "@/dto/sign-in.dto";
+
+import { richFetch } from "@/utils/fetch.utils";
+
 import styles from "./sign-in.module.css";
+
+type SigninType = {
+  username: string;
+  password: string;
+};
+
+async function postSigninApi(signinData: SigninType): Promise<SigninDto> {
+  const data = await richFetch<SigninDto>("/auth/sign-in", {
+    method: "POST",
+    body: JSON.stringify(signinData),
+  });
+
+  if ("error" in data) {
+    throw new Error(data.error);
+  }
+
+  return data.result;
+}
 
 export default function SignInPage(): ReactNode {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [checked, setChecked] = useState(false);
+
+  const mutation = useMutation({
+    mutationKey: ["sign-in"],
+    mutationFn: postSigninApi,
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      await mutation.mutateAsync({ username, password });
+      navigate("/");
+      toast("You sign-in successfully!");
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+  };
 
   return (
     <div className={styles["sign-in"]}>
@@ -21,7 +67,9 @@ export default function SignInPage(): ReactNode {
         title="Welcome"
       >
         <div className={styles.logo}>Game Name</div>
-        <form>
+        <form
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}
+        >
           <div className={styles.fields}>
             <label>
               Your Name
@@ -49,7 +97,7 @@ export default function SignInPage(): ReactNode {
             checked={checked}
             onChange={(e) => setChecked(e.target.checked)}
           />
-          <LinkButtonComponent to="/signup" color="light" size="small">
+          <LinkButtonComponent to="/sign-up" color="light" size="small">
             Sign Up
           </LinkButtonComponent>
           <ButtonComponent>Login</ButtonComponent>
--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -1,85 +1,117 @@
-import { type ReactNode, useState } from "react";
+import { type FormEvent, type ReactNode, useState } from "react";
+
+import { useNavigate } from "react-router";
+
+import { useMutation } from "@tanstack/react-query";
+
+import { toast } from "sonner";
 
 import ButtonComponent from "@/components/button/button.component";
+import CheckboxComponent from "@/components/checkbox/checkbox.component";
 import LinkButtonComponent from "@/components/link-button/link-button.component";
 import PaneComponent from "@/components/pane/pane.component";
 import TextInputComponent from "@/components/text-input/text-input.component";
 
+import type { SignupDto } from "@/dto/sign-up.dto";
+
+import { richFetch } from "@/utils/fetch.utils";
+
 import styles from "./sign-up.module.css";
 
+type SignupType = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+async function postSignupApi(signupData: SignupType): Promise<SignupDto> {
+  const data = await richFetch<SignupDto>("/auth/sign-up", {
+    method: "POST",
+    body: JSON.stringify(signupData),
+  });
+
+  if ("error" in data) {
+    throw new Error(data.error);
+  }
+
+  return data.result;
+}
 export default function SignUpPage(): ReactNode {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [checked, setChecked] = useState(false);
 
+  const mutation = useMutation({
+    mutationKey: ["sign-up"],
+    mutationFn: postSignupApi,
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      await mutation.mutateAsync({ username, email, password });
+      navigate("/sign-in");
+      toast("You created account successfully!");
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+  };
+
   return (
-    <div className={styles.parent}>
+    <div className={styles["sign-up"]}>
       <PaneComponent
-        shade
-        key={"signup"}
-        title="welcome"
-        className={styles["pane-signup-page"]}
+        className={styles.pane}
+        contentClassName={styles.container}
+        title="Welcome"
       >
-        <div className={styles.container}>
-          <h2 className={styles["game-name"]}>G A M E &nbsp;N A M E</h2>
-          <div className={styles["input-fields"]}>
-            <div className={styles["input-box"]}>
-              <label htmlFor="username">YOUR NAME</label>
+        <div className={styles.logo}>Game Name</div>
+        <form
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}
+        >
+          <div className={styles.fields}>
+            <label>
+              Your Name
               <TextInputComponent
+                className={styles.input}
                 name="username"
-                id="username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                className={styles["input-style"]}
               />
-            </div>
-            <div className={styles["input-box"]}>
-              <label htmlFor="password">PASSWORD</label>
+            </label>
+            <label>
+              Password
               <TextInputComponent
+                className={styles.input}
                 name="password"
-                id="password"
+                type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                className={styles["input-style"]}
               />
-            </div>
-            <div className={styles["input-box"]}>
-              <label htmlFor="email">EMAIL</label>
+            </label>
+            <label>
+              Email
               <TextInputComponent
+                className={styles.input}
                 name="email"
-                id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                className={styles["input-style"]}
               />
-            </div>
+            </label>
           </div>
-          <div className={styles.buttons}>
-            <div className={styles["checkbox-field"]}>
-              <label className={styles["checkbox-label"]}>remember me</label>
-              <input
-                type="checkbox"
-                checked={checked}
-                className={styles["checkbox-input"]}
-                onChange={(e) => setChecked(e.target.checked)}
-              />
-            </div>
-            <LinkButtonComponent
-              to={"/signin"}
-              className={styles["signin-link"]}
-            >
-              LOGIN
-            </LinkButtonComponent>
-          </div>
-          <div className={styles["signup-button-parent"]}>
-            <ButtonComponent className={styles["signup-button"]}>
-              SIGN UP
-            </ButtonComponent>
-          </div>
-        </div>
+          <CheckboxComponent
+            className={styles.checkbox}
+            label="Remember Me"
+            checked={checked}
+            onChange={(e) => setChecked(e.target.checked)}
+          />
+          <LinkButtonComponent to={"/sign-in"} color="light" size="small">
+            login
+          </LinkButtonComponent>
+          <ButtonComponent>Sign Up</ButtonComponent>
+        </form>
       </PaneComponent>
     </div>
   );
