fae33f6ede2b4d73591071ada4f5618068e6a58d
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-07-21 08:25:10 +0330

    [ADD] edit profile route

--- src/components/router/router.component.tsx
+++ src/components/router/router.component.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 
 import { BrowserRouter, Route, Routes } from "react-router";
 
+import EditProfilePage from "@/pages/edit-profile/edit-profile.page.tsx";
 import GamePage from "@/pages/game/game.page.tsx";
 import IntroPage from "@/pages/intro/intro.page.tsx";
 import PlaygroundPage from "@/pages/playground/playground.page.tsx";
@@ -13,6 +14,7 @@ export default function RouterComponent(): ReactNode {
         <Route path="/" element={<IntroPage />} />
         <Route path="/game" element={<GamePage />} />
         <Route path="/playground" element={<PlaygroundPage />} />
+        <Route path="/edit-profile" element={<EditProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
