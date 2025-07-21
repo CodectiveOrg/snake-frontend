539d3e8b15af5610d2ecde93b33dee36539b6940
zeinabvishkayee <zeinabvishkayee@gmail.com>
2025-07-21 19:20:43 +0330

    fix the  router

--- src/components/router/router.component.tsx
+++ src/components/router/router.component.tsx
@@ -6,10 +6,8 @@ import GuestOnlyGuard from "@/guards/guest-only.guard.tsx";
 import LoggedInOnlyGuard from "@/guards/logged-in-only.guard.tsx";
 
 import GamePage from "@/pages/game/game.page.tsx";
-
-import HomePage from "@/pages/home/home.page";
+import HomePage from "@/pages/home/home.page.tsx";
 import ModalPage from "@/pages/modal/modal.page.tsx";
-
 import PlaygroundPage from "@/pages/playground/playground.page.tsx";
 import QueryPage from "@/pages/query/query.page.tsx";
 
@@ -17,12 +15,14 @@ export default function RouterComponent(): ReactNode {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route element={<GuestOnlyGuard />}>
+          <Route path="/sign-in" element={<h1>Sign In</h1>} />
+          <Route path="/sign-up" element={<h1>Sign Up</h1>} />
+        </Route>
         <Route element={<LoggedInOnlyGuard />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/game" element={<GamePage />} />
         </Route>
-
         <Route path="/query" element={<QueryPage />} />
         <Route path="/modal" element={<ModalPage />} />
         <Route path="/playground" element={<PlaygroundPage />} />
