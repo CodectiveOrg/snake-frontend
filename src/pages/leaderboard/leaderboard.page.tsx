0da2a3dbc243833dbf99d06a6212137ee1b6574e
ali <moradkhani.mohammadali@gmail.com>
2025-07-22 18:03:30 +0330

    Add image and button

--- src/pages/leaderboard/leaderboard.page.tsx
+++ src/pages/leaderboard/leaderboard.page.tsx
@@ -1,5 +1,8 @@
 import type { ReactNode } from "react";
 
+import { useNavigate } from "react-router";
+
+import ButtonComponent from "@/components/button/button.component";
 import PaneComponent from "@/components/pane/pane.component";
 import TableComponent from "@/components/table/table.component";
 
@@ -12,12 +15,36 @@ const items = [
     todayHighScore: 12,
     totalHighScore: 90,
   },
+  {
+    rank: 3,
+    username: "Mohammmad",
+    todayHighScore: 12,
+    totalHighScore: 90,
+  },
+  {
+    rank: 3,
+    username: "Mohammmad",
+    todayHighScore: 12,
+    totalHighScore: 90,
+  },
 ];
 
 export default function LeaderboardPage(): ReactNode {
+  const navigate = useNavigate();
+
+  const backHandler = (): Promise<void> | void => navigate("/");
+
   return (
-    <PaneComponent title="Board" className={styles.leaderboard}>
+    <PaneComponent
+      title="Board"
+      contentClassName={styles.content}
+      className={styles.leaderboard}
+    >
+      <div className={styles["user-image"]}>
+        <img src="/images/user-picture-placeholder.webp" />
+      </div>
       <TableComponent items={items} />
+      <ButtonComponent onClick={backHandler}>Back</ButtonComponent>
     </PaneComponent>
   );
 }
