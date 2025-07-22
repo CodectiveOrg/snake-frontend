8fce694b83feb6defd3ca2569d23b8b695c7627d
Sina Amini <91621769+SinaMAmini@users.noreply.github.com>
2025-07-22 17:31:37 +0330

    Implement Guide Page (#21)
    * [INIT] guide page
    
    * [ADD] images and initial styles
    
    * [NEW] esc button
    
    * [FIX] img placement
    
    * [REWRITE] explaining
    
    * [FIX] grids
    
    * [ADD] button
    
    * [ADD] footer
    
    * [FIX] heights
    
    * [FIX] route conflict
    
    * [FIX] reactnode type
    
    * [FIX] wrong routing
    
    * [FIX] pr issues
    
    * [ADD] link variant to button component
    
    ---------
    
    Co-authored-by: BijanProgrammer <bijaneisapour@gmail.com>

--- src/components/router/router.component.tsx
+++ src/components/router/router.component.tsx
@@ -6,6 +6,7 @@ import GuestOnlyGuard from "@/guards/guest-only.guard.tsx";
 import LoggedInOnlyGuard from "@/guards/logged-in-only.guard.tsx";
 
 import GamePage from "@/pages/game/game.page.tsx";
+import GuidePage from "@/pages/guide/guide.page.tsx";
 import IntroPage from "@/pages/intro/intro.page.tsx";
 import ModalPage from "@/pages/modal/modal.page.tsx";
 import PlaygroundPage from "@/pages/playground/playground.page.tsx";
@@ -16,16 +17,19 @@ export default function RouterComponent(): ReactNode {
     <BrowserRouter>
       <Routes>
         <Route element={<GuestOnlyGuard />}>
-          <Route path="/sign-in" element={<h1>Sign In</h1>} />
-          <Route path="/sign-up" element={<h1>Sign Up</h1>} />
+          <Route path="sign-in" element={<h1>Sign In</h1>} />
+          <Route path="sign-up" element={<h1>Sign Up</h1>} />
         </Route>
         <Route element={<LoggedInOnlyGuard />}>
-          <Route path="/" element={<IntroPage />} />
-          <Route path="/game" element={<GamePage />} />
+          <Route path="" element={<IntroPage />} />
+          <Route path="game" element={<GamePage />} />
         </Route>
-        <Route path="/query" element={<QueryPage />} />
-        <Route path="/modal" element={<ModalPage />} />
-        <Route path="/playground" element={<PlaygroundPage />} />
+        <Route path="playground">
+          <Route index element={<PlaygroundPage />} />
+          <Route path="query" element={<QueryPage />} />
+          <Route path="modal" element={<ModalPage />} />
+        </Route>
+        <Route path="guide" element={<GuidePage />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </BrowserRouter>
