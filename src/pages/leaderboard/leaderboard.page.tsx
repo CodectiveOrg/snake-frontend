8a3ea190b883ab3c6461598e1ef43849c741e8de
ali <moradkhani.mohammadali@gmail.com>
2025-07-22 18:27:31 +0330

    Connect to leaderboard API

--- src/pages/leaderboard/leaderboard.page.tsx
+++ src/pages/leaderboard/leaderboard.page.tsx
@@ -1,6 +1,13 @@
 import type { ReactNode } from "react";
+import React from "react";
 
 import { useNavigate } from "react-router";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { toast } from "react-toastify";
+
+import { getLeaderboardApi } from "@/api/public/get-leaderboard.api.ts";
 
 import ButtonComponent from "@/components/button/button.component";
 import PaneComponent from "@/components/pane/pane.component";
@@ -8,31 +15,30 @@ import TableComponent from "@/components/table/table.component";
 
 import styles from "./leaderboard.module.css";
 
-const items = [
-  {
-    rank: 3,
-    username: "Mohammmad",
-    todayHighScore: 12,
-    totalHighScore: 90,
-  },
-  {
-    rank: 3,
-    username: "Mohammmad",
-    todayHighScore: 12,
-    totalHighScore: 90,
-  },
-  {
-    rank: 3,
-    username: "Mohammmad",
-    todayHighScore: 12,
-    totalHighScore: 90,
-  },
-];
-
 export default function LeaderboardPage(): ReactNode {
   const navigate = useNavigate();
 
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["leaderboard"],
+    queryFn: getLeaderboardApi,
+  });
+
   const backHandler = (): Promise<void> | void => navigate("/");
+
+  const pendingMessage = (): React.JSX.Element | undefined => {
+    return <p className={styles.message}>Loading...</p>;
+  };
+
+  const errorMessage = (): React.JSX.Element | undefined => {
+    if (isError) {
+      toast.error(error.message, {
+        containerId: "modal",
+        toastId: "user-rank",
+      });
+
+      return <p className={styles.message}>Error: {error.message}</p>;
+    }
+  };
 
   return (
     <PaneComponent
@@ -40,11 +46,19 @@ export default function LeaderboardPage(): ReactNode {
       contentClassName={styles.content}
       className={styles.leaderboard}
     >
-      <div className={styles["user-image"]}>
-        <img src="/images/user-picture-placeholder.webp" />
-      </div>
-      <TableComponent items={items} />
-      <ButtonComponent onClick={backHandler}>Back</ButtonComponent>
+      {isPending ? (
+        pendingMessage()
+      ) : isError ? (
+        errorMessage()
+      ) : (
+        <React.Fragment>
+          <div className={styles["user-image"]}>
+            <img src="/images/user-picture-placeholder.webp" />
+          </div>
+          <TableComponent items={data} />
+          <ButtonComponent onClick={backHandler}>Back</ButtonComponent>
+        </React.Fragment>
+      )}
     </PaneComponent>
   );
 }
