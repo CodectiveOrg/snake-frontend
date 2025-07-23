1d4e5d3e171e329051f0971383a59de3c3ce886e
mazaherireza <rezamazaheri.email@gmail.com>
2025-07-24 00:01:37 +0330

    [ADD] complete design

--- src/pages/user/components/row/row.component.tsx
+++ src/pages/user/components/row/row.component.tsx
@@ -1,17 +1,24 @@
 import type { ReactNode } from "react";
 
+import clsx from "clsx";
+
 import styles from "./row.module.css";
 
 type Props = {
-  key: string;
+  title: string;
   value: string;
+  className?: string;
 };
 
-export default function RowComponent({ key, value }: Props): ReactNode {
+export default function RowComponent({
+  title,
+  value,
+  className,
+}: Props): ReactNode {
   return (
     <div className={styles.row}>
-      <div className={styles.key}>{key}</div>
-      <div className={styles.value}>{value}</div>
+      <div className={styles.title}>{title}</div>
+      <div className={clsx(styles.value, className)}>{value}</div>
     </div>
   );
 }
--- src/pages/user/user.page.tsx
+++ src/pages/user/user.page.tsx
@@ -1,22 +1,38 @@
 import type { ReactNode } from "react";
 
+import ButtonComponent from "@/components/button/button.component";
 import PaneComponent from "@/components/pane/pane.component";
+
+import RowComponent from "./components/row/row.component";
 
 import styles from "./user.module.css";
 
 export default function UserPage(): ReactNode {
   return (
     <div className={styles.user}>
-      <PaneComponent title="Player">
-        <div className={styles.wrapper}>
-          <div>
-            <img
-              className={styles.img}
-              src="images/user-picture-placeholder.webp"
-              alt="Profile"
-            />
-          </div>
-          <div></div>
+      <PaneComponent
+        shade
+        className={styles.pane}
+        contentClassName={styles.content}
+        title="Player"
+      >
+        <div>
+          <img
+            className={styles.img}
+            src="images/user-picture-placeholder.webp"
+            alt="Profile"
+          />
+        </div>
+        <div>
+          <RowComponent title="name" value="AK46" className={styles.bold} />
+          <RowComponent title="ranking" value="43" />
+          <RowComponent title="total score" value="808" />
+          <RowComponent title="gender" value="male" className={styles.bold} />
+
+          <ButtonComponent className={styles.button}>
+            {" "}
+            &lt;- Back
+          </ButtonComponent>
         </div>
       </PaneComponent>
     </div>
