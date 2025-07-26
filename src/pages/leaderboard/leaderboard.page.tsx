d49d92f202c2c16b28fa322475ae3b0c2398d680
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-26 14:26:52 +0330

    [ADD] top player component

--- src/pages/leaderboard/leaderboard.page.tsx
+++ src/pages/leaderboard/leaderboard.page.tsx
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router";
 
 import { useQuery } from "@tanstack/react-query";
 
-import clsx from "clsx";
-
 import { getLeaderboardApi } from "@/api/history/get-leaderboard.api";
 
 import ButtonComponent from "@/components/button/button.component";
 import PaneComponent from "@/components/pane/pane.component";
 import TableComponent from "@/components/table/table.component";
+
+import TopPlayersComponent from "@/pages/leaderboard/components/top-players/top-players.component.tsx";
 
 import styles from "./leaderboard.module.css";
 
@@ -40,25 +40,7 @@ export default function LeaderboardPage(): ReactNode {
           <div>Error: {error.message}</div>
         ) : (
           <>
-            <div className={styles["top-players"]}>
-              {data.slice(1, 4).map((player, index) => (
-                <div
-                  key={index}
-                  className={clsx(
-                    styles.player,
-                    styles[`player-${["one", "two", "three"][index]}`],
-                  )}
-                >
-                  <div className={styles["image-box"]}>
-                    <img src="/images/user-picture-placeholder.webp" />
-                  </div>
-                  <div className={styles.overlay}>
-                    <span className={styles.username}>{player.username}</span>
-                    <span>{player.totalHighScore}</span>
-                  </div>
-                </div>
-              ))}
-            </div>
+            <TopPlayersComponent players={data?.slice(1, 4)} />
             <TableComponent items={data.slice(1)} />
             <div className={styles.actions}>
               <ButtonComponent onClick={backButtonClickHandler}>
