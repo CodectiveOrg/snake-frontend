7e7ef6fe79a5ef10731ca5353652d00a8653e829
mazaherireza <rezamazaheri.email@gmail.com>
2025-07-26 17:09:02 +0330

    [ADD] api

--- src/pages/user/user.page.tsx
+++ src/pages/user/user.page.tsx
@@ -1,5 +1,11 @@
 import type { ReactNode } from "react";
 
+import { useParams } from "react-router";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { getUserPublicInfoApi } from "@/api/user/get-user-public-info";
+
 import ButtonComponent from "@/components/button/button.component";
 import PaneComponent from "@/components/pane/pane.component";
 
@@ -8,6 +14,27 @@ import RowComponent from "./components/row/row.component";
 import styles from "./user.module.css";
 
 export default function UserPage(): ReactNode {
+  const params = useParams();
+  const username = params.username!;
+
+  const {
+    data: userData,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["user-public-info", username],
+    queryFn: () => getUserPublicInfoApi(username),
+  });
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className={styles.user}>
       <PaneComponent
@@ -23,11 +50,20 @@ export default function UserPage(): ReactNode {
             alt="Profile"
           />
         </div>
+
         <div>
-          <RowComponent title="name" value="AK46" className={styles.bold} />
+          <RowComponent
+            title="name"
+            value={userData.username}
+            className={styles.bold}
+          />
           <RowComponent title="ranking" value="43" />
           <RowComponent title="total score" value="808" />
-          <RowComponent title="gender" value="male" className={styles.bold} />
+          <RowComponent
+            title="gender"
+            value={userData.gender}
+            className={styles.bold}
+          />
 
           <ButtonComponent className={styles.button}>
             {" "}
