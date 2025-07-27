07a6a510a62abd95ec24ecb7a5e3228baa89db9f
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-27 09:03:54 +0330

    [RESPONSIVE] pane frame's dents

--- src/components/pane/pane.component.tsx
+++ src/components/pane/pane.component.tsx
@@ -25,6 +25,9 @@ export default function PaneComponent({
   contentClassName,
   children,
 }: Props): ReactNode {
+  const { ref: paneRef, width: paneWidth } =
+    useResizeObserverHook<HTMLDivElement>();
+
   const { ref: titleRef, width: titleWidth } =
     useResizeObserverHook<HTMLDivElement>();
 
@@ -35,9 +38,11 @@ export default function PaneComponent({
     2 * Size.PANE_CORNER_OFFSET;
 
   const minWidth = titleWidth + minBleed;
+  const hasDent = paneWidth >= 600;
 
   return (
     <div
+      ref={paneRef}
       className={clsx(styles.pane, className)}
       style={
         {
@@ -46,9 +51,17 @@ export default function PaneComponent({
         } as CSSProperties
       }
     >
-      <PaneFrameComponent className={styles.frame} titleWidth={titleWidth} />
+      <PaneFrameComponent
+        className={styles.frame}
+        titleWidth={titleWidth}
+        hasDent={hasDent}
+      />
       {shade && (
-        <PaneShadeComponent className={styles.shade} titleWidth={titleWidth} />
+        <PaneShadeComponent
+          className={styles.shade}
+          titleWidth={titleWidth}
+          hasDent={hasDent}
+        />
       )}
       <div ref={titleRef} className={styles.title}>
         {title}
