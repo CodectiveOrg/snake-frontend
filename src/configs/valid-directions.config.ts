c3b7af84226ab74b1649bd4f386f8265819e5e5e
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-07-28 11:11:26 +0330

    [ADD] arrow keys

--- src/configs/valid-directions.config.ts
+++ src/configs/valid-directions.config.ts
@@ -1,8 +1,12 @@
 import type { DirectionType } from "@/types/direction.type.ts";
 
 export const VALID_DIRECTIONS: Record<DirectionType, DirectionType[]> = {
-  KeyW: ["KeyA", "KeyD"],
-  KeyA: ["KeyW", "KeyS"],
-  KeyS: ["KeyA", "KeyD"],
-  KeyD: ["KeyW", "KeyS"],
+  KeyW: ["KeyA", "KeyD", "ArrowRight", "ArrowLeft"],
+  KeyA: ["KeyW", "KeyS", "ArrowUp", "ArrowDown"],
+  KeyS: ["KeyA", "KeyD", "ArrowRight", "ArrowLeft"],
+  KeyD: ["KeyW", "KeyS", "ArrowUp", "ArrowDown"],
+  ArrowUp: ["KeyA", "KeyD", "ArrowRight", "ArrowLeft"],
+  ArrowLeft: ["KeyW", "KeyS", "ArrowUp", "ArrowDown"],
+  ArrowDown: ["KeyA", "KeyD", "ArrowRight", "ArrowLeft"],
+  ArrowRight: ["KeyW", "KeyS", "ArrowUp", "ArrowDown"],
 };
--- src/types/direction.type.ts
+++ src/types/direction.type.ts
@@ -1,3 +1,12 @@
-export const DIRECTIONS = ["KeyW", "KeyA", "KeyS", "KeyD"] as const;
+export const DIRECTIONS = [
+  "KeyW",
+  "KeyA",
+  "KeyS",
+  "KeyD",
+  "ArrowUp",
+  "ArrowRight",
+  "ArrowDown",
+  "ArrowLeft",
+] as const;
 
 export type DirectionType = (typeof DIRECTIONS)[number];
