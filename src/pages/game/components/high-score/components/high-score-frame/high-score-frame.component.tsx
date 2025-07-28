e37749302d5af9ad9c75aefcb8ff1dbfe5d2e496
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-28 16:44:43 +0330

    [RESPONSIVE] high score frame

--- src/pages/game/components/high-score/components/high-score-frame/high-score-frame.component.tsx
+++ src/pages/game/components/high-score/components/high-score-frame/high-score-frame.component.tsx
@@ -6,46 +6,49 @@ import styles from "./high-score-frame.module.css";
 
 type Props = SVGProps<SVGSVGElement> & {
   highScoreWidth: number;
+  highScoreHeight: number;
 };
 
 export default function HighScoreFrameComponent({
   highScoreWidth,
+  highScoreHeight,
   className,
   ...otherProps
 }: Props): ReactNode {
   const extraWidth = highScoreWidth - 30;
+  const height = highScoreHeight + 18;
 
   return (
     <svg
       className={clsx(styles["high-score-frame"], className)}
       style={{
         width: `${120 + extraWidth}px`,
-        height: "59px",
+        height: `${height}px`,
       }}
-      viewBox={`0 0 ${120 + extraWidth} 59`}
+      viewBox={`0 0 ${120 + extraWidth} ${height}`}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...otherProps}
     >
       <path
         className={styles.pane}
-        d={`M${101.027 + extraWidth} 0.5L${78.649 + extraWidth} 58.5H18.9736L41.3516 0.5H${101.027 + extraWidth}Z`}
+        d={`M${101.027 + extraWidth} 0.5L${78.649 + extraWidth} ${height - 0.5}H18.9736L41.3516 0.5H${101.027 + extraWidth}Z`}
       />
       <path
         className={styles.perimeter}
-        d="M31.6058 0H38.1465L15.6628 59H9.12207L31.6058 0Z"
+        d={`M31.6058 0H38.1465L15.6628 ${height}H9.12207L31.6058 0Z`}
       />
       <path
         className={styles.perimeter}
-        d="M22.4837 0H29.0244L6.54071 59H0L22.4837 0Z"
+        d={`M22.4837 0H29.0244L6.54071 ${height}H0L22.4837 0Z`}
       />
       <path
         className={styles.perimeter}
-        d={`M${113.459 + extraWidth} 0H${120 + extraWidth}L${97.516 + extraWidth} 59H${90.976 + extraWidth}L${113.459 + extraWidth} 0Z`}
+        d={`M${113.459 + extraWidth} 0H${120 + extraWidth}L${97.516 + extraWidth} ${height}H${90.976 + extraWidth}L${113.459 + extraWidth} 0Z`}
       />
       <path
         className={styles.perimeter}
-        d={`M${104.337 + extraWidth} 0H${110.878 + extraWidth}L${88.394 + extraWidth} 59H${81.854 + extraWidth}L${104.337 + extraWidth} 0Z`}
+        d={`M${104.337 + extraWidth} 0H${110.878 + extraWidth}L${88.394 + extraWidth} ${height}H${81.854 + extraWidth}L${104.337 + extraWidth} 0Z`}
       />
     </svg>
   );
